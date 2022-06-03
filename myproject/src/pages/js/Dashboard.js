d10e5efdf310e6b59287aa65094b9f14e691d56b
diy0r4ik <95129772+diy0r4ik@users.noreply.github.com>
2022-06-03 11:56:15 +0500

    svdac

--- myproject/src/pages/js/Dashboard.js
+++ myproject/src/pages/js/Dashboard.js
@@ -5,6 +5,11 @@ import img1 from '../img/Icon.png'
 import img2 from '../img/Rectangle.png'
 import img3 from '../img/card1.png'
 import img4 from '../img/main3.png'
+import img5 from '../img/main4.png'
+import img6 from '../img/men.png'
+import img7 from '../img/women.png'
+import img8 from '../img/oral_shop.png'
+import img9 from '../img/guy.png'
 export default class Dashboard extends Component{
     render(){
         return(
@@ -76,7 +81,65 @@ export default class Dashboard extends Component{
                 <a className='main3_link' href=''>Show me how it works</a> 
               </div>
               <div className='main4'>
-                <div className='main4_box'></div>
+                <div className='main4_box'>
+                    <img src={img5} alt=" " className='main4-img'/>
+                    <div className='left_text'>
+                    <h1 className='left_title'>Refillable Discbound Journals</h1>
+                    <p className='left_p'>Heirloom quality you'll be holding onto for life</p>
+                    <a className='left_a' href=''>Shop the collection →</a>
+                  </div>
+                </div>
+              </div>
+              <div className='main5'>
+                <h1 className='main5_title'>Our mission is to create simple, beautiful objects that combine form and function.</h1>
+                <div className='main5_cards'>
+                  <div className='men_card'>
+                    <img src={img6} alt=" " className='men-img'/>
+                    <a className='card_a' href=''>Shop Mens →</a>
+                  </div>
+                
+                  <div className='women_card'>
+                    <img src={img7} alt=" " className='women-img'/>
+                    <a className='card_a' href=''>Shop Womens →</a>  
+                  </div>
+                </div>
+                <div className='oral_shop'>
+                  <div className='oral_shop_card'>
+                    <img src={img8} alt=" " className='oral_shop-img'/>
+                    <a className='card_a' href=''>Shop Objects →</a>
+                  </div>
+                </div>
+              </div>
+              <div className='main6'>
+                <img src={img9} alt=" " className='guy-img'/>
+                <h1 className='main6_title'>A design studio in Downingtown, PA, creating and curating products that combine form & function</h1>
+                <a href=''>Read Our Story →</a>
+              </div>
+              <div className='main7'>
+                <h1 className='main7_title'>Stay in the loop</h1>
+                <p className='main6_p'>Be the first to know when new products drop and get behind-the-scenes content straight from Ugmonk’s founder.</p>
+                <div className='main7_box'></div>
+              </div>
+              <div className='footer'>
+                <ul className='footer_left'>
+                  <li a href=''>Customer Service</li>
+                  <li a href=''>Customer Service</li>
+                  <li a href=''>Customer Service</li>
+                  <li a href=''>Customer Service</li>
+                  <li a href=''>Customer Service</li>
+                  <li a href=''>Customer Service</li>
+                </ul>
+                <ul className='footer_right'>
+                  <li a href=''>About</li>
+                  <li a href=''>About</li>
+                  <li a href=''>About</li>
+                  <li a href=''>About</li>
+                  <li a href=''>About</li>
+                  <li a href=''>About</li>
+                </ul>
+                <div className='footer_info'>
+                <img src={img} alt=" " className='logo-img'/>
+                </div>
               </div>
             </div>
         )
