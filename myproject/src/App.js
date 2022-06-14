23d92f4b9e5bbcaa3485a1989aaa726770caba6a
diy0r4ik <95129772+diy0r4ik@users.noreply.github.com>
2022-06-14 18:01:52 +0500

    nxcfgjkdtk
    dtukdtukdtyk

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -2,7 +2,7 @@ import React, {Component,} from "react";
 import "../src/App.css"
 // import Dashboard from "./pages/js/Dashboard";
 // import State from "./pages/js/state";
-import Json from "./json";
+import Baza from "./pages/js/Baza";
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default class App extends Component{
 
@@ -11,7 +11,7 @@ export default class App extends Component{
       <div>
         {/* <Dashboard/> */}
         {/* <State/> */}
-        <Json/>
+        <Baza/>
       </div>
     )
   }
---
+++ myproject/src/pages/js/Baza.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+const items=require('./.././../data')
+// import{ Nav, Navbar,NavDropdown, Container, Form, Button}from "react-bootstrap"
+
+
+export default function App(){
+
+
+  return (
+    <div>
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    <div className="Malika_mumacha">
+        {data.map(item=>{
+            return <Card className="cards">
+      <Card.Img variant="top" src={items.Img}/>
+      <Card.Body>
+        <Card.Title>items.title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>{items.price}</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+        })}
+    
+    {/* <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://i.pinimg.com/originals/0a/d4/4e/0ad44ebdf52a3168c52362afa1eb8fdb.jpg" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://i.pinimg.com/originals/0a/d4/4e/0ad44ebdf52a3168c52362afa1eb8fdb.jpg" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card> */}
+    </div>
+        </div>
+    )
+}
